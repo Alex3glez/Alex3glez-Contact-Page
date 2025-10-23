95f0dfc8874c91fb69cdea123c7d30651d643a89
Alex <a4l3e2x1@hotmail.com>
2025-10-23 05:15:14 +0200

    añadidos navbar, app

---
+++ Alex3glez-contact-page/src/services/contactServices.js
@@ -0,0 +1,108 @@
+export const getAgendas = async() => {
+    try {
+        const request= await fetch("https://playground.4geeks.com/contact/agendas?offset=0&limit=100");
+        const response = await request.json();
+        console.log(response);
+        return response;
+    } catch (error) { console.log(error);
+    
+        
+    }
+  }
+
+  
+  export const getAgenda = async(name) => {
+    try {
+        const request= await fetch(`https://playground.4geeks.com/contact/agendas/${name}`);
+        const response= await request.json();
+        console.log(response);
+        return response;
+    } catch (error) { console.log(error);
+    
+        
+    }
+  }
+
+  export const deleteAgenda = async(name) => {
+    try {
+        const request = await fetch(`https://playground.4geeks.com/contact/agendas/${name}?tags=Agenda%20operations&summary=Delete%20Agenda.&description=Deletes%20a%20specific%20agenda%20from%20the%20database.`, {
+            method: "DELETE",
+            headers: {'accept': 'application/json'}}
+        )
+        if (!request.ok) {
+            throw new error(request.statusText)
+        
+        }
+                
+    } catch (error) { throw error;
+    
+        
+    }
+  }
+
+  export const createAgenda = async(name) => {
+    try {
+        const request= await fetch(`https://playground.4geeks.com/contact/agendas/${name}`,{
+            method: "POST",
+            headers: {'accept': 'application/json'},
+            body:""
+        })
+        if (!request.ok) {
+            throw new error(request.statusText)
+        
+        }
+                
+    } catch (error) { throw error;
+    
+        
+    }
+  }
+
+  export const createAgendaContact = async(agendaName, newContact) => {
+    try {
+        const request=await fetch(`https://playground.4geeks.com/contact/agendas/${agendaName}/contacts`,{
+            method: "POST",
+            headers: {'accept': 'application/json',
+                'content-type': 'application/json'},
+            body: JSON.stringify(newContact) 
+            })
+            
+    } catch (error) { console.log(error);
+    
+        
+    }
+  }
+
+  export const updateAgendaContact = async(agendaName, contactId, contactUpdates) => {
+    try {
+        const request= await fetch(`https://playground.4geeks.com/contact/agendas/${agendaName}/contacts/${contactId}`,{
+            method: "PUT",
+            headers:{
+                'accept': 'application/json',
+                'content-type': 'application/json'},
+                body: JSON.stringify(contactUpdates)//si en el objeto contactUpdates existe algúna propiedad undefined, con stringify no se incluye por lo que en la api se queda el valor anterior.
+            }
+        )
+        const response = await request.json();
+        console.log(response)
+    } catch (error) {
+        
+    }
+  }
+  
+ export const deleteAgendaContact = async(agendaName, contactId) => {
+    try {
+        const request=await fetch(`https://playground.4geeks.com/contact/agendas/${agendaName}/contacts/${contactId}`,{
+            method: "DELETE",
+            headers: {'accept': 'application/json'}
+        })
+        if (!request.ok) {
+            throw new error(request.statusText)
+        
+        }
+    } catch (error) { throw error;
+        
+    }
+ }
+
+
